0ea91153bd791f4248023a3f5c49a9f9bec83cc9
Saurabh Singh Bisht <bisht.singh750@gmail.com>
2023-04-25 20:39:40 +0530

    Make Product class public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,8 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(5,10);
-        p.product(5,10,15);
-        p.product(5.5,5.10);
+        System.out.println(p.product(5,10));
+        System.out.println(p.product(5,10,15));
+        System.out.println(p.product(5.5,5.10));
     }
 }
